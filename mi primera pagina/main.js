7d1e55b4d7f2a7fd4f20155d44105dfb31dd06f8
Aramm27 <132170256+Aramm27@users.noreply.github.com>
2023-04-29 16:06:14 -0600

    Add files via upload

---
+++ mi primera pagina/main.js
@@ -0,0 +1,17 @@
+let app = documento.getElemenByld('typewriter');
+let typewriter = new Typewrite (app,{
+loop: true,
+delay:75,
+}),
+typewriter
+.pause Form (2500)
+.typeString('desarrollo de sitios web y escribo articulos sobre programacion')
+.pauseForm(200)
+.delateChars(10)
+.start();
+/* contacto */
+.contacto .container {
+    max - width; 1100; px;
+    min - heigth ; 200; px; 
+    padding ; 20; px;
+}
